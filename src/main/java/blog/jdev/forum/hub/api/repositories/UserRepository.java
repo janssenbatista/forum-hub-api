5400c4cfebfb4e237c3be276f2d8e3cef0663deb
Janssen Batista <batistajanssen.dev@gmail.com>
2024-07-02 11:44:56 -0300

    feat: create user repository

---
+++ src/main/java/blog/jdev/forum/hub/api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package blog.jdev.forum.hub.api.repositories;
+
+import blog.jdev.forum.hub.api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
