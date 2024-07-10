d3d40c6dae8f09cf56845a268572af3a655d10ee
Janssen Batista <batistajanssen.dev@gmail.com>
2024-07-10 18:20:57 -0300

    refactor: remove unused method

--- src/main/java/blog/jdev/forum/hub/api/controllers/dtos/TopicResponseDTO.java
+++ src/main/java/blog/jdev/forum/hub/api/controllers/dtos/TopicResponseDTO.java
@@ -1,8 +1,6 @@
 package blog.jdev.forum.hub.api.controllers.dtos;
 
-import blog.jdev.forum.hub.api.models.Topic;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -11,7 +9,4 @@ public record TopicResponseDTO(UUID id, String title, String message,
                                @JsonFormat
                                        (shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
                                LocalDateTime createdAt) {
-    public TopicResponseDTO toResponseDTO(Topic topic) {
-        return new TopicResponseDTO(topic.getId(), topic.getTitle(), topic.getMessage(), topic.getCreatedAt());
-    }
 }
